b4d9ca5b70594427e83ab624f70dad4d895f8050
Rohith <gambol99@gmail.com>
2018-03-08 22:38:38 +0000

    - default the accounts to zero for now - making the dns zone a requirement - switching to using arn instead of names - removing the environment from the record as it's implied in the domain

--- main.tf
+++ main.tf
@@ -5,30 +5,25 @@
  *        source         = "git::https://github.com/UKHomeOffice/acp-tf-endpoint-service?ref=master"
  *
  *        name            = "internal-ingress"
- *        alb_name        = "test"
+ *        alb_arn         = "arn:name"
  *        environment     = "dev"            # by default both Name and Env is added to the tags
  *        dns_zone        = "example.com"
  *      }
  *
  */
 
-# Get the network alb we are attached to
-data "aws_lb" "selected" {
-  name = "${var.alb_name}"
-}
-
-# Get the host zone id
+## Get the host zone id
 data "aws_route53_zone" "selected" {
   name = "${var.dns_zone}."
 }
 
-# Create the endpoint service
+## Create the endpoint service
 resource "aws_vpc_endpoint_service" "service" {
   acceptance_required        = "${var.acceptance_required}"
-  network_load_balancer_arns = ["${data.aws_lb.selected.arn}"]
+  network_load_balancer_arns = ["${var.alb_arn}"]
 }
 
-# Permit the accounts access to the endpoint service
+## Permit the accounts access to the endpoint service
 resource "aws_vpc_endpoint_service_allowed_principal" "permitted" {
   count = "${length(var.aws_accounts)}"
 
@@ -36,11 +31,9 @@ resource "aws_vpc_endpoint_service_allowed_principal" "permitted" {
   vpc_endpoint_service_id = "${aws_vpc_endpoint_service.service.id}"
 }
 
-# Create a DNS record if required
+## Create a DNS record if required
 resource "aws_route53_record" "dns" {
-  count = "${dns_zone == "" ? 0 : 1}"
-
-  name    = "${var.name}-${var.environment}-endpoint-svc"
+  name    = "${var.name}-endpoint"
   records = ["${aws_vpc_endpoint_service.service.private_dns_name}"]
   ttl     = "30"
   type    = "CNAME"
